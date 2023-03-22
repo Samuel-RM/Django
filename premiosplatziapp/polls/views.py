0b90ddcf89b5c7a71eb6f8901eb50ae63c7f2b9f
Samuel-RM <samiriverame07@gmail.com>
2023-03-22 16:51:14 -0600

    configurando view y otras cosas

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,17 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
 from django.http import HttpResponse
 
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("Estas en la pagina principal")
+    lates_question_list = Question.objects.all()
+    return render(request, "polls/index.html/", {
+        "lates_question_list":lates_question_list
+    })
 
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+    question = get_object_or_404(Question, PK=question_id)
+    return render(request, "polls/detail.html/", {
+        "question":question
+    })
 
 def result(request, question_id):
     return HttpResponse(f"Estas viendo los resultados de la pregunta  {question_id}")
 
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando la pregunta numero{question_id}")
-
+    return HttpResponse(f"Estas votando la pregunta numero  {question_id}")
