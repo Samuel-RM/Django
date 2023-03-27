b4bff89c8d307730965bf4f0d6bf0d4c38c30080
Samuel-RM <samiriverame07@gmail.com>
2023-03-27 13:58:33 -0600

    Modificando las view

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     #ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     #ex: /polls/5
-    path('<int:question_id>/detail/dasdad', views.detail, name='detail'),
+    path('<int:pk>/detail/dasdad', views.DetailView.as_view(), name='detail'),
     #ex: /polls/5/results
-    path('<int:question_id>/result/', views.result, name='result'),
+    path('<int:pk>/result/', views.ResultlView.as_view(), name='result'),
     #ex: /polls/5/vote
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
